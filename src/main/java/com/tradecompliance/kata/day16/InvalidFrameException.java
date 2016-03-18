bc41975a409ace7e0f47350e2801210e5223600f
AsaithambiV <Vinoth.Asaithambi@accuity.com>
2016-03-18 10:15:01 +0000

    Day16 - same exception as Day15

---
+++ src/main/java/com/tradecompliance/kata/day16/InvalidFrameException.java
@@ -0,0 +1,17 @@
+package com.tradecompliance.kata.day16;
+
+public class InvalidFrameException extends IllegalArgumentException {
+    static final long serialVersionUID = -2848938806368998894L;
+
+    public InvalidFrameException() {
+        super();
+    }
+
+    public InvalidFrameException(String s) {
+        super (s);
+    }
+
+    static InvalidFrameException forInputString(String s) {
+        return new InvalidFrameException("For input string: \"" + s + "\"");
+    }
+}
